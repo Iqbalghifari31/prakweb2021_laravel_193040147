2443c082cfb444b022cc1aaaf17d5d95d505bbe5
Mohammad Iqbal Ghifari <60709403+Iqbalghifari31@users.noreply.github.com>
2021-10-15 15:53:00 +0700

    upload video 5. Model, Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,56 +33,8 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Iqbal Ghifari",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem natus totam cum hic explicabo mollitia, ut magni eligendi voluptatem aliquid vitae voluptate. Repellat eveniet illum quo ullam aut vero corporis, obcaecati distinctio deserunt asperiores beatae rem! Sint nulla, fuga iste consequuntur neque cum, voluptatibus sed, dolores dignissimos quidem delectus eveniet dicta cupiditate quasi veniam exercitationem explicabo blanditiis vero error? Id quidem consequatur, tempora laborum temporibus illum necessitatibus accusamus amet exercitationem excepturi quaerat atque reprehenderit maxime adipisci est porro nisi perspiciatis."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Tatang Solihun",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque veritatis, quidem debitis nobis error laborum maxime repudiandae consectetur, laudantium voluptatum, libero unde recusandae autem expedita labore alias veniam amet perferendis a sunt quam rem nemo dolorum. Quaerat omnis suscipit impedit aliquid sequi id, ipsam blanditiis aspernatur nulla eveniet aut deserunt at necessitatibus accusamus illo voluptatum itaque. Reiciendis, minima? Laboriosam impedit expedita deleniti illum cum exercitationem atque ipsum dolorem possimus, doloremque, laborum rem porro animi consequuntur ex ducimus dolorum obcaecati velit itaque. Neque pariatur quis, ullam quam architecto corporis excepturi provident rem exercitationem debitis, aliquam, nobis reprehenderit culpa aspernatur rerum incidunt.
-            "
-        ]
-        ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 // halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Iqbal Ghifari",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem natus totam cum hic explicabo mollitia, ut magni eligendi voluptatem aliquid vitae voluptate. Repellat eveniet illum quo ullam aut vero corporis, obcaecati distinctio deserunt asperiores beatae rem! Sint nulla, fuga iste consequuntur neque cum, voluptatibus sed, dolores dignissimos quidem delectus eveniet dicta cupiditate quasi veniam exercitationem explicabo blanditiis vero error? Id quidem consequatur, tempora laborum temporibus illum necessitatibus accusamus amet exercitationem excepturi quaerat atque reprehenderit maxime adipisci est porro nisi perspiciatis."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Tatang Solihun",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque veritatis, quidem debitis nobis error laborum maxime repudiandae consectetur, laudantium voluptatum, libero unde recusandae autem expedita labore alias veniam amet perferendis a sunt quam rem nemo dolorum. Quaerat omnis suscipit impedit aliquid sequi id, ipsam blanditiis aspernatur nulla eveniet aut deserunt at necessitatibus accusamus illo voluptatum itaque. Reiciendis, minima? Laboriosam impedit expedita deleniti illum cum exercitationem atque ipsum dolorem possimus, doloremque, laborum rem porro animi consequuntur ex ducimus dolorum obcaecati velit itaque. Neque pariatur quis, ullam quam architecto corporis excepturi provident rem exercitationem debitis, aliquam, nobis reprehenderit culpa aspernatur rerum incidunt.
-            "
-        ]
-        ];
-
-        $new_post = [];
-        foreach($blog_posts as $post){
-            if($post["slug"] === $slug) {
-                $new_post = $post;
-            }
-        }
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-    
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
 
