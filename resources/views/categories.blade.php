d82a4db4a4cfb5f7f51d7358943a31999ebb5749
Mohammad Iqbal Ghifari <60709403+Iqbalghifari31@users.noreply.github.com>
2021-10-29 12:34:07 +0700

    menyelesaikan video 12 (Redesign UI) - laravel 8

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,14 +2,25 @@
 
 @section('container')
     <h1 class="mb-5">Post Categories</h1>
-    <br>
+    
+    <div class="container">
+        <div class="row">
+            @foreach ($category as $category)
+            <div class="col-md-4">
+                <a href="/categories/{{$category->slug}}">
+                <div class="card bg-dark text-white">
+                <img src="https://source.unsplash.com/500x500?{{$category->name}}" class="card-img" alt="{{$category->name}}" >
+                    <div class="card-img-overlay d-flex align-items-center p-0">
+                        <h5 class="card-title flex-fill text-center p-4 fs-3" style="background-color: rgba(0,0,0,0.7)"
+                            href="/categories/{{ $category->slug }}" class="text-decoration-none text-white">{{ $category->name }}
+                        </h5>
+                    </div>
+                </div>
+                </a>
+            </div>
+        @endforeach
 
-    @foreach ($category as $category)
-        <ul>
-            <li>
-                <h2><a href="/categories/{{ $category->slug }}">{{ $category->name }}</a></h2>
-            </li>
-        </ul>
+        </div>
+    </div>
 
-    @endforeach
 @endsection
