99208df704e09715324473d68e59f15ef406d850
Mohammad Iqbal Ghifari <60709403+Iqbalghifari31@users.noreply.github.com>
2021-11-26 02:25:17 +0700

    menyelesaikan video 13 (Searching & Pagination) - laravel 8

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,23 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $title = '';
+        if(request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+        if(request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' in ' . $author->name;
+        }
         return view('posts', [
-            "title" => "All Postss",
-            "active" => "Posts",
-            // "posts" => Post::all()
-            "posts" => Post::latest()->get()
-        ]); 
+            "title" => "All Posts" . $title,
+            "active" => "Blog",
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
+        ]);
     }
 
     public function show(Post $post) {
         return view('post', [
             "title" => "Single Post",
-            "active" => "Posts",
+            "active" => "Blog",
             "post" => $post
         ]);
     }
